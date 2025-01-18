a03a77e22742c2750ad7bdde505dc01cab858771
Teforch <matveev.gleb.boy@gmail.com>
2025-01-18 18:22:12 +0300

    add validation

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -2,14 +2,19 @@ import express from "express";
 import UserController from "../controllers/user.contoller.js";
 import isGuestMiddleware from "../middlewares/isGuest.middleware.js";
 import userActionsMiddleware from "../middlewares/userActions.middleware.js";
+import validationMiddleware from "../middlewares/validation.middleware.js";
 
 const router = express.Router();
 
 router.get("/users", isGuestMiddleware, UserController.getUsers);
-router.post("/users", UserController.createUser);
+router.post("/users", validationMiddleware, UserController.createUser);
 router.delete("/users/:id", userActionsMiddleware, UserController.deleteUser);
-router.get('/users/:id/edit', userActionsMiddleware, isGuestMiddleware, UserController.editPage)
-router.patch('/users/:id', userActionsMiddleware,UserController.updateUser)
+router.get('/users/:id/edit', userActionsMiddleware, isGuestMiddleware, UserController.editPage);
+router.patch('/users/:id', 
+    userActionsMiddleware,
+    (req, res, next) => validationMiddleware(req, res, next, `/users/${req.params.id}/edit`),
+    UserController.updateUser);
+
 router.get('/users/new', isGuestMiddleware, UserController.registerPage);
 
 
